af33fe9ac9e897fdf637ef99edaf72e93b724afc
Phil Flanagan <phileflanagan@gmail.com>
2016-10-15 10:39:01 -0400

    Update UI/Ux and implement an option for 2 OS

--- select-plan-angular/js/index.js
+++ select-plan-angular/js/index.js
@@ -1,15 +1,16 @@
 (function(){
 	'use strict';
 	angular.module('app', []);
-	
+
 	angular.module('app')
 		.controller('serverCtrl', serverCtrl);
-	
+
 	function serverCtrl() {
 		var vm = this;
-		
+
 		vm.linuxOptions = [
 			{
+				os: 'linux',
 				name: 'VPS 512MB',
 				cpu: '1 v-Core',
 				ram: '512 MB',
@@ -18,6 +19,7 @@
 				price: '5'
 			},
 			{
+				os: 'linux',
 				name: 'VPS 1',
 				cpu: '1 v-Core',
 				ram: '1 GB',
@@ -26,6 +28,7 @@
 				price: '10'
 			},
 			{
+				os: 'linux',
 				name: 'VPS 2',
 				cpu: '2 v-Core',
 				ram: '2 GB',
@@ -34,6 +37,7 @@
 				price: '20'
 			},
 			{
+				os: 'linux',
 				name: 'VPS 3',
 				cpu: '4 v-Core',
 				ram: '3 GB',
@@ -42,6 +46,7 @@
 				price: '40'
 			},
 			{
+				os: 'linux',
 				name: 'VPS 4',
 				cpu: '5 v-Core',
 				ram: '4 GB',
@@ -50,27 +55,97 @@
 				price: '60'
 			},
 			{
+				os: 'linux',
 				name: 'VPS 5',
 				cpu: '6 v-Core',
 				ram: '5 GB',
 				disk: '70 GB',
 				bandwidth: 'Unlimited',
 				price: '80'
+			}];
+
+		vm.windowsOptions = [
+			{
+				os: 'windows',
+				name: 'VPS 1',
+				cpu: '2 v-Core',
+				ram: '1.5 GB',
+				disk: '15 GB',
+				bandwidth: 'Unlimited',
+				price: '20'
 			},
-			
-		];
+			{
+				os: 'windows',
+				name: 'VPS 2',
+				cpu: '3 v-Core',
+				ram: '2 GB',
+				disk: '30 GB',
+				bandwidth: 'Unlimited',
+				price: '25'
+			},
+			{
+				os: 'windows',
+				name: 'VPS 3',
+				cpu: '4 v-Core',
+				ram: '4 GB',
+				disk: '40 GB',
+				bandwidth: 'Unlimited',
+				price: '40'
+			},
+			{
+				os: 'windows',
+				name: 'VPS 4',
+				cpu: '5 v-Core',
+				ram: '6 GB',
+				disk: '50 GB',
+				bandwidth: 'Unlimited',
+				price: '60'
+			},
+			{
+				os: 'windows',
+				name: 'VPS 5',
+				cpu: '6 v-Core',
+				ram: '8 GB',
+				disk: '60 GB',
+				bandwidth: 'Unlimited',
+				price: '80'
+			}];
+
+		// Defaults and Initalization Values
+		vm.os = 'linux';
 		vm.selection = 1;
 		vm.selectedPlan = vm.linuxOptions[vm.selection - 1];
-		
+
+		vm.setOS = function(os) {
+			vm.os = os;
+			vm.selection = 1; //avoid out of bounds index possibility
+			vm.adjustSlider();
+		}
+
 		vm.adjustSlider = function() {
-			console.log()
-			var adjLeft = 'calc(calc(100% - 100px) / 5 * '+ (vm.selection - 1) +')';
+			var adjLeft;
+			if (vm.os === 'linux') adjLeft = 'calc(calc(100% - 100px) / 5 * '+ (vm.selection - 1) +')';
+			if(vm.os === 'windows') adjLeft = 'calc(calc(100% - 100px) / 4 * '+ (vm.selection - 1) +')'
 			angular.element('.slider span').css({'left': adjLeft})
 			updateInfo();
 		}
 		
+		/*
+		 * Goes goes here to handle the order
+		 */
+		vm.handleOrder = function() {
+			var order = vm.selectedPlan; //JavaScript Object Literal
+			var orderJSON = JSON.stringify(order); //JSON Data
+			
+			// Selected Plan
+			console.log(orderJSON);
+			
+		}
+		
+		// Utility Functions
 		function updateInfo() {
-			vm.selectedPlan = vm.linuxOptions[vm.selection - 1];
+			if (vm.os === 'linux') vm.selectedPlan = vm.linuxOptions[vm.selection - 1];
+			if (vm.os === 'windows') vm.selectedPlan = vm.windowsOptions[vm.selection - 1];
 		}
 	}
 }());
